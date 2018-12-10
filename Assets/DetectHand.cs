c54c783c567e4af31e5f849f203f4a5967f73857
Takaya Ogawa <bumpofchicken344@gmail.com>
2018-12-11 01:02:38 +0900

    get maxAreaCentroid

--- Assets/DetectHand.cs
+++ Assets/DetectHand.cs
@@ -12,7 +12,7 @@ public class DetectHand : MonoBehaviour
     WebCamTexture _webcamtex;
     WebCamTextureToMatHelper _webCamTextureToMatHelper;
     FpsMonitor _fpsMonitor;
-    Mat _rgbMat, _hand, _handMask, _label, _contour;
+    Mat _rgbMat, _hand, _handMask;
     
     static readonly Scalar SKIN_LOWER = new Scalar(0, 70, 90);
     static readonly Scalar SKIN_UPPER = new Scalar(35, 255, 255);
@@ -20,8 +20,51 @@ public class DetectHand : MonoBehaviour
     void Start()
     {
         _fpsMonitor = GetComponent<FpsMonitor>();
-        _webCamTextureToMatHelper = gameObject.GetComponent<WebCamTextureToMatHelper>();
+        _webCamTextureToMatHelper = GetComponent<WebCamTextureToMatHelper>();
         _webCamTextureToMatHelper.Initialize();
+    }
+    
+    void Update()
+    {
+        if (!_webCamTextureToMatHelper.IsPlaying() || !_webCamTextureToMatHelper.DidUpdateThisFrame()) return;
+        
+        var rgbaMat = _webCamTextureToMatHelper.GetMat();
+        Detect(rgbaMat);
+        Utils.fastMatToTexture2D(_hand, _texture);
+    }
+
+    void Detect(Mat rgbaMat)
+    {
+        //RGBAをHSVに変換
+        cvtColor(rgbaMat, _rgbMat, COLOR_RGBA2RGB);
+        cvtColor(_rgbMat, _rgbMat, COLOR_RGB2HSV);
+        
+        //肌色領域を抽出
+        inRange(_rgbMat, SKIN_LOWER, SKIN_UPPER, _handMask);
+        morphologyEx(_handMask, _handMask, MORPH_OPEN, new Mat(), new Point(-1, -1), 3);
+        morphologyEx(_handMask, _handMask, MORPH_CLOSE, new Mat(), new Point(-1, -1), 3);
+
+        //ラベリング
+        var centroids = new Mat();
+        var stats = new Mat();
+        var nLabels = connectedComponentsWithStats(_handMask, new Mat(), stats, centroids);
+
+        //最大の領域の重心を取得
+        var maxAreaIndex = 0;
+        var maxArea = 0.0;
+        for (int i = 1; i < nLabels; i++) {        //0番目は背景のため飛ばす
+            var area = stats.get(i, CC_STAT_AREA); //https: //teratail.com/questions/112820
+            if (area[0] > maxArea) {
+                maxArea = area[0];
+                maxAreaIndex = i;
+            }
+        }
+        var maxAreaCentroid = new Point(centroids.get(maxAreaIndex, 0)[0], centroids.get(maxAreaIndex, 1)[0]);
+        print(maxAreaCentroid);
+        
+        cvtColor(_handMask, _rgbMat, COLOR_GRAY2RGB);
+        circle(_rgbMat, maxAreaCentroid, 30, new Scalar(255, 0, 0), -1);
+        cvtColor(_rgbMat, _hand, COLOR_RGB2RGBA);
     }
     
     public void OnWebCamTextureToMatHelperInitialized ()
@@ -55,8 +98,6 @@ public class DetectHand : MonoBehaviour
         _rgbMat = new Mat (webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC3);
         _hand = new Mat (webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC4);
         _handMask = new Mat (webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC1);
-        _label = new Mat(webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_32S);
-        _contour = new Mat (webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC3);
     }
 
     public void OnWebCamTextureToMatHelperDisposed ()
@@ -66,55 +107,6 @@ public class DetectHand : MonoBehaviour
             Destroy(_texture);
             _texture = null;
         }
-    }
-    
-    void Update()
-    {
-        if (!_webCamTextureToMatHelper.IsPlaying() || !_webCamTextureToMatHelper.DidUpdateThisFrame()) return;
-        
-        var rgbaMat = _webCamTextureToMatHelper.GetMat();
-        Detect(rgbaMat);
-        Utils.fastMatToTexture2D(_hand, _texture);
-    }
-
-    void Detect(Mat rgbaMat)
-    {
-        //RGBAをHSVに変換
-        cvtColor(rgbaMat, _rgbMat, COLOR_RGBA2RGB);
-        cvtColor(_rgbMat, _rgbMat, COLOR_RGB2HSV);
-        
-        //肌色領域を抽出
-        inRange(_rgbMat, SKIN_LOWER, SKIN_UPPER, _handMask);
-        morphologyEx(_handMask, _handMask, MORPH_OPEN, new Mat(), new Point(-1, -1), 3);
-        morphologyEx(_handMask, _handMask, MORPH_CLOSE, new Mat(), new Point(-1, -1), 3);
-
-        //ラベリングして重心を取得
-        var centroids = new Mat();
-        var stats = new Mat();
-        var nLabels = connectedComponentsWithStats(_handMask, _label, stats, centroids);
-
-        //最大の領域の重心を取得
-        var maxAreaIndex = 0;
-        var maxArea = 0.0;
-        for (int i = 1; i < nLabels; i++) {    //0番目は背景のため飛ばす
-            var area = stats.get(i, CC_STAT_AREA);
-            if (area[0] > maxArea) {
-                maxArea = area[0];
-                maxAreaIndex = i;
-            }
-        }
-        var maxAreaCentroid = new Point(centroids.row(maxAreaIndex).get(0, 0));
-//        var maxAreaCentroid = centroids.row(maxAreaIndex).get(0, 0);
-        print(maxAreaCentroid);
-
-//        var contours = new List<MatOfPoint>();
-//        findContours(_handMask, contours, new Mat(), RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-//        var contour = new Mat(rgbaMat.rows(), rgbaMat.cols(), CvType.CV_8UC3);
-//        drawContours(contour, contours, -1, new Scalar(255, 0, 0));
-//        Debug.Log(contours.Count);
-        
-        cvtColor(_handMask, _rgbMat, COLOR_GRAY2RGB);
-        cvtColor(_rgbMat, _hand, COLOR_RGB2RGBA);
     }
     
     void OnDestroy()
