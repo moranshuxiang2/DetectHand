94296df6851ee6b710d3ff99cfac93754b8dbfbd
Takaya Ogawa <bumpofchicken344@gmail.com>
2018-12-11 03:04:26 +0900

    show fire

--- Assets/DetectHand.cs
+++ Assets/DetectHand.cs
@@ -6,22 +6,32 @@ using static OpenCVForUnity.Imgproc;
 using static OpenCVForUnity.Core;
 
 [RequireComponent(typeof(WebCamTextureToMatHelper), typeof(FpsMonitor))]
-public class DetectHand : MonoBehaviour 
+public class DetectHand : MonoBehaviour
 {
+    [SerializeField] ParticleSystem _firePrefab;
+    
     Texture2D _texture;
     WebCamTexture _webcamtex;
     WebCamTextureToMatHelper _webCamTextureToMatHelper;
     FpsMonitor _fpsMonitor;
-    Mat _rgbMat, _hand, _handMask;
+    ParticleSystem _fire;
     
     static readonly Scalar SKIN_LOWER = new Scalar(0, 70, 90);
     static readonly Scalar SKIN_UPPER = new Scalar(35, 255, 255);
+    static readonly float FIRE_SCALE = 100f;
+    static readonly float FIRE_Z_POS = -10f;
     
     void Start()
     {
+        Input.backButtonLeavesApp = true;
+        
         _fpsMonitor = GetComponent<FpsMonitor>();
         _webCamTextureToMatHelper = GetComponent<WebCamTextureToMatHelper>();
         _webCamTextureToMatHelper.Initialize();
+//        GetComponent<Renderer>().material.mainTexture = _webCamTextureToMatHelper.GetWebCamTexture();
+        
+        _fire = Instantiate(_firePrefab, new Vector3(0f, 0f, FIRE_Z_POS), Quaternion.identity);
+        _fire.transform.localScale = new Vector3(FIRE_SCALE, FIRE_SCALE, FIRE_SCALE);
     }
     
     void Update()
@@ -29,42 +39,46 @@ public class DetectHand : MonoBehaviour
         if (!_webCamTextureToMatHelper.IsPlaying() || !_webCamTextureToMatHelper.DidUpdateThisFrame()) return;
         
         var rgbaMat = _webCamTextureToMatHelper.GetMat();
-        Detect(rgbaMat);
-        Utils.fastMatToTexture2D(_hand, _texture);
+        SetFire(rgbaMat);
+//        _texture = _webCamTextureToMatHelper.GetWebCamTexture();
+        Utils.fastMatToTexture2D(rgbaMat, _texture);
     }
 
-    void Detect(Mat rgbaMat)
+    void SetFire(Mat rgbaMat)
     {
         //RGBAをHSVに変換
-        cvtColor(rgbaMat, _rgbMat, COLOR_RGBA2RGB);
-        cvtColor(_rgbMat, _rgbMat, COLOR_RGB2HSV);
+        var rgbMat = new Mat();
+        cvtColor(rgbaMat, rgbMat, COLOR_RGBA2RGB);
+        cvtColor(rgbMat, rgbMat, COLOR_RGB2HSV);
         
         //肌色領域を抽出
-        inRange(_rgbMat, SKIN_LOWER, SKIN_UPPER, _handMask);
-        morphologyEx(_handMask, _handMask, MORPH_OPEN, new Mat(), new Point(-1, -1), 3);
-        morphologyEx(_handMask, _handMask, MORPH_CLOSE, new Mat(), new Point(-1, -1), 3);
+        var handMask = new Mat();
+        inRange(rgbMat, SKIN_LOWER, SKIN_UPPER, handMask);
+        morphologyEx(handMask, handMask, MORPH_OPEN, new Mat(), new Point(-1, -1), 3);
+        morphologyEx(handMask, handMask, MORPH_CLOSE, new Mat(), new Point(-1, -1), 3);
 
         //ラベリング
         var centroids = new Mat();
         var stats = new Mat();
-        var nLabels = connectedComponentsWithStats(_handMask, new Mat(), stats, centroids);
+        var nLabels = connectedComponentsWithStats(handMask, new Mat(), stats, centroids);
 
         //最大の領域の重心を取得
         var maxAreaIndex = 0;
         var maxArea = 0.0;
-        for (int i = 1; i < nLabels; i++) {        //0番目は背景のため飛ばす
+        for (int i = 1; i < nLabels; i++) {        //0番目のラベルは背景のため飛ばす
             var area = stats.get(i, CC_STAT_AREA); //https: //teratail.com/questions/112820
             if (area[0] > maxArea) {
                 maxArea = area[0];
                 maxAreaIndex = i;
             }
         }
-        var maxAreaCentroid = new Point(centroids.get(maxAreaIndex, 0)[0], centroids.get(maxAreaIndex, 1)[0]);
-        print(maxAreaCentroid);
+        //https://stackoverflow.com/questions/34470927/connectedcomponentswithstats-return-types-and-values-in-java
+        var ctrd = new Point(centroids.get(maxAreaIndex, 0)[0], centroids.get(maxAreaIndex, 1)[0]);
+        var ctrdWorldPos = new Point((float)ctrd.x - rgbaMat.width() / 2f, rgbaMat.height() / 2f - (float)ctrd.y);
+        var firePos = new Vector3((float)ctrdWorldPos.x, (float)ctrdWorldPos.y, FIRE_Z_POS);
         
-        cvtColor(_handMask, _rgbMat, COLOR_GRAY2RGB);
-        circle(_rgbMat, maxAreaCentroid, 30, new Scalar(255, 0, 0), -1);
-        cvtColor(_rgbMat, _hand, COLOR_RGB2RGBA);
+        //炎を手の位置に移動
+        _fire.transform.position = firePos;
     }
     
     public void OnWebCamTextureToMatHelperInitialized ()
@@ -73,9 +87,9 @@ public class DetectHand : MonoBehaviour
 
         var webCamTextureMat = _webCamTextureToMatHelper.GetMat();
         _texture = new Texture2D (webCamTextureMat.cols(), webCamTextureMat.rows(), TextureFormat.RGBA32, false);
-        gameObject.GetComponent<Renderer>().material.mainTexture = _texture;
+        GetComponent<Renderer>().material.mainTexture = _texture;
         
-        gameObject.transform.localScale = new Vector3 (webCamTextureMat.cols(), webCamTextureMat.rows(), 1);
+        transform.localScale = new Vector3 (webCamTextureMat.cols(), webCamTextureMat.rows(), 1);
         Debug.Log ("Screen.width " + Screen.width + " Screen.height " + Screen.height + " Screen.orientation " + Screen.orientation);
 
         if (_fpsMonitor != null){
@@ -84,8 +98,8 @@ public class DetectHand : MonoBehaviour
             _fpsMonitor.Add ("orientation", Screen.orientation.ToString());
         }
         
-        float width = webCamTextureMat.width ();
-        float height = webCamTextureMat.height ();
+        float width = webCamTextureMat.width();
+        float height = webCamTextureMat.height();
                                 
         float widthScale = Screen.width / width;
         float heightScale = Screen.height / height;
@@ -94,10 +108,6 @@ public class DetectHand : MonoBehaviour
         } else {
             Camera.main.orthographicSize = height / 2;
         }
-        
-        _rgbMat = new Mat (webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC3);
-        _hand = new Mat (webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC4);
-        _handMask = new Mat (webCamTextureMat.rows(), webCamTextureMat.cols(), CvType.CV_8UC1);
     }
 
     public void OnWebCamTextureToMatHelperDisposed ()
